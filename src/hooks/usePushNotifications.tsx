4626bb90ed305ebe8cfbc5c9ccb4a9621abd31d6
ukemike <ukemike4@gmail.com>
2024-10-29 10:35:44 +0100

    fixes

--- src/hooks/usePushNotifications.tsx
+++ src/hooks/usePushNotifications.tsx
@@ -1,16 +1,67 @@
+// import { useEffect, useState } from "react";
+// import messaging from "@react-native-firebase/messaging";
+// // import { Alert } from "react-native";
+// // import * as Clipboard from "expo-clipboard";
+
+// const usePushNotifications = () => {
+//   const [token, setToken] = useState<any>(null);
+//   const [error, setError] = useState<any>(null);
+
+//   // const copyToClipboard = (token: string) => {
+//   //   Clipboard.setStringAsync(token);
+//   //   Alert.alert("Copied to clipboard", token);
+//   // };
+
+//   useEffect(() => {
+//     const requestUserPermission = async () => {
+//       try {
+//         const authStatus = await messaging().requestPermission();
+//         const enabled =
+//           authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+//           authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+//         if (enabled) {
+//           console.log("Authorization push notification authorized");
+//         } else {
+//           console.log("Authorization push notification rejected");
+//         }
+//       } catch (error) {
+//         console.error("Error getting notification permission", error);
+//       }
+//     };
+
+//     const getTokenAndRegister = async () => {
+//       await requestUserPermission();
+//       try {
+//         const token = await messaging().getToken();
+//         setToken(token);
+//         // console.log("Push token", token);
+//         // Alert.alert("Push Token", token, [
+//         //   { text: "Cancel", style: "cancel" },
+//         //   { text: "Copy", onPress: () => copyToClipboard(token) },
+//         // ]);
+//       } catch (error) {
+//         console.error("Error getting push token", error);
+//         setError(error);
+//       }
+//     };
+
+//     getTokenAndRegister();
+//   }, []);
+
+//   return { token, error };
+// };
+
+// export default usePushNotifications;
+
+
+
 import { useEffect, useState } from "react";
 import messaging from "@react-native-firebase/messaging";
-// import { Alert } from "react-native";
-// import * as Clipboard from "expo-clipboard";
 
-const usePushNotifications = () => {
+const usePushNotification = () => {
   const [token, setToken] = useState<any>(null);
   const [error, setError] = useState<any>(null);
-
-  // const copyToClipboard = (token: string) => {
-  //   Clipboard.setStringAsync(token);
-  //   Alert.alert("Copied to clipboard", token);
-  // };
 
   useEffect(() => {
     const requestUserPermission = async () => {
@@ -30,18 +81,30 @@ const usePushNotifications = () => {
       }
     };
 
+    // const getTokenAndRegister = async () => {
+    //   await requestUserPermission();
+    //   try {
+    //     const token = await messaging().getToken();
+    //     setToken(token);
+    //   } catch (error) {
+    //     console.error("Error getting push token", error);
+    //     setError(error);
+    //   }
+    // };
+
     const getTokenAndRegister = async () => {
       await requestUserPermission();
+
       try {
+        // Register the device for remote messages
+        await messaging().registerDeviceForRemoteMessages();
+
+        // Get the device token
         const token = await messaging().getToken();
         setToken(token);
-        // console.log("Push token", token);
-        // Alert.alert("Push Token", token, [
-        //   { text: "Cancel", style: "cancel" },
-        //   { text: "Copy", onPress: () => copyToClipboard(token) },
-        // ]);
+        console.log("Device FCM Token:", token);
       } catch (error) {
-        console.error("Error getting push token", error);
+        console.error("Error getting push token:", error);
         setError(error);
       }
     };
@@ -52,4 +115,4 @@ const usePushNotifications = () => {
   return { token, error };
 };
 
-export default usePushNotifications;
+export default usePushNotification;
